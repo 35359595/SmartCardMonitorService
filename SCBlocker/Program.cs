130a18e2cc82f88c480b5929a988475ccc31d1b4
ivan.temchenko <ivan.temchenko@yandex.ua>
2017-06-04 13:47:00 +0200

    SC Removed Event Monitor

---
+++ SCBlocker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace SCBlocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<SCManager>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new SCManager());
+                    serviceInstance.WhenStarted(s => s.StartMonitoring());
+                    serviceInstance.WhenStopped(s => s.StopMonitoring());
+                });
+                serviceConfig.SetServiceName("scRemoveLock");
+                serviceConfig.StartManually();
+            });
+        }
+    }
+}
---
+++ SCBlocker/SCManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCSC;
+
+namespace SCBlocker
+{
+    class SCManager
+    {
+        private string[] _readers;
+        private SCardMonitor _monitor = new SCardMonitor(ContextFactory.Instance, SCardScope.System);
+
+        public SCManager()
+        {
+            using (var context = new SCardContext())
+            {
+                context.Establish(SCardScope.System);
+                _readers = context.GetReaders();
+            }
+        }
+
+        private void _sCardRemoved(object sender, CardStatusEventArgs e)
+        {
+            Console.WriteLine("Locking machine. SmartCard was removed.");
+            // LockWorkStation();
+        }
+
+        public void StartMonitoring()
+        {
+            _monitor.CardRemoved += new CardRemovedEvent(_sCardRemoved);
+            foreach (string reader in _readers)
+                _monitor.Start(reader);
+        }
+
+        public void StopMonitoring()
+        {
+            _monitor = new SCardMonitor(ContextFactory.Instance, SCardScope.System);
+        }
+    }
+}
